525ce6b9e3051eb21e95f4f63cd13d86b40261a8
cherry880807 <cherry880807@yahoo.com.tw>
2019-10-18 13:35:23 +0800

    Initial Commit

---
+++ 00557147_hw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00557147_hw2
+//
+//  Created by User02 on 2019/10/18.
+//  Copyright © 2019 User02. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
